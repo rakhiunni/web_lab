341e0db9744d9ddc54bee1cf287c2271bfce5782
rakhiunni <56077218+rakhiunni@users.noreply.github.com>
2019-12-12 22:51:48 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -96,6 +96,21 @@ run();
     
     })
 
+    window.addEventListener('load', function() {
+        var status = document.getElementById("status");
+      
+        function updateOnlineStatus(event) {
+          var condition = navigator.onLine ? "online" : "offline";
+      
+          status.className = condition;
+          status.innerHTML = condition.toUpperCase();
+      
+        }
+      
+        window.addEventListener('online',  updateOnlineStatus);
+        window.addEventListener('offline', updateOnlineStatus);
+      });
+
 
 
 
